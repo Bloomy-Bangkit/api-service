a526eb9973af0796e2b908b41c4f2b9ee537b8ac
Aditya Bayu <97433258+iniadittt@users.noreply.github.com>
2023-12-09 21:21:27 +0700

    updated: add env from google secret manager

--- src/application/server.js
+++ src/application/server.js
@@ -45,13 +45,6 @@ app.get('/', (req, res) => {
             Moto: 'Cihhh! Jangan meremehkan wibuuu, dasar Ninggen tidak bergunaa! >.< iKuzooo minnaa..',
             CreatedBy: 'Aditya Bayu',
             Copyright: '©2023 All Rights Reserved!'
-        },
-        env: {
-            DB_NAME: process.env.DB_NAME,
-            DB_HOST: process.env.DB_HOST,
-            DB_USER: process.env.DB_USER,
-            DB_PASS: process.env.DB_PASS,
-            DB_PORT: process.env.DB_PORT,
         }
     })
 })
