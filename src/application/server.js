ba03cdab6ba7fe729236651e0a5dd59e41c98192
Aditya Bayu <97433258+iniadittt@users.noreply.github.com>
2023-12-17 21:28:53 +0700

    update: error condition in location controller response

--- src/application/server.js
+++ src/application/server.js
@@ -42,6 +42,7 @@ app.use(rateLimit({
 }))
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/auth', authRoute)
--- src/location/location-controller.js
+++ src/location/location-controller.js
@@ -3,7 +3,7 @@ const locationService = require('./location-service.js')
 const getProvinsi = async(req, res, next) => {
     try {
         const result = await locationService.getProvinsi()
-        return res.status(200).json({ message: 'Berhasil get data provinsi', data: result })
+        return res.status(200).json({ error: false, message: 'Berhasil get data provinsi', data: result })
     } catch (error) {
         next(error)
     }
@@ -13,7 +13,7 @@ const getKota = async(req, res, next) => {
     try {
         const namaProvinsi = req.params.nama
         const result = await locationService.getKota(namaProvinsi)
-        return res.status(200).json({ message: 'Berhasil get data kota', data: result })
+        return res.status(200).json({ error: false, message: 'Berhasil get data kota', data: result })
     } catch (error) {
         next(error)
     }
